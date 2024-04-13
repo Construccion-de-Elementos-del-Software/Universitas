05dea94e7e2eec59bc096d562435639db6d88adc
Santiago9j <santitorresaguirre@gmail.com>
2024-04-12 19:45:16 -0500

    create method get users

--- src/main/java/co/edu/poli/ces/universitas/database/ConexionMysql.java
+++ src/main/java/co/edu/poli/ces/universitas/database/ConexionMysql.java
@@ -1,8 +1,6 @@
 package co.edu.poli.ces.universitas.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConexionMysql {
     // Importante: Tener XAMM, una aplicación que empaqueta varios servicios
@@ -24,7 +22,7 @@ public class ConexionMysql {
         nameDatabase = "universitas";
     }
 
-    public Connection createConexion(){
+    public void createConexion(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             cnn = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+nameDatabase, user, password);
@@ -33,12 +31,34 @@ public class ConexionMysql {
             System.out.println("An error occurred during the connection.");
             throw new RuntimeException(e);
         }
-        return  null;
     }
 
+    public void getUsers() throws SQLException {
+        String sql = "SELECT * FROM USERS";
+        try {
+            createConexion();
+            Statement stmt = cnn.createStatement();
+            ResultSet result = stmt.executeQuery(sql);
+
+            while (result.next()){
+                System.out.println("***************************");
+                System.out.println("ID: "+result.getInt(1));
+                System.out.println("Name: "+result.getString(2));
+                System.out.println("Lastname: "+result.getString("lastName"));
+                System.out.println("****************************");
+            }
+            stmt.close();
+            result.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            cnn.close();
+        }
+
+    }
     public static void main(String[] args) {
         ConexionMysql conection = new ConexionMysql();
-        conection.createConexion();
+        conection.getUsers();
     }
 
 
