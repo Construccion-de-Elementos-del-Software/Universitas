20caab1d913e2a36975facbf2e6211c2c7471042
Santiago9j <santitorresaguirre@gmail.com>
2024-04-13 21:38:33 -0500

    jcreate methos insert and update

--- src/main/java/co/edu/poli/ces/universitas/database/ConexionMysql.java
+++ src/main/java/co/edu/poli/ces/universitas/database/ConexionMysql.java
@@ -60,9 +60,84 @@ public class ConexionMysql {
         }
 
     }
+
+    public void createUser(String name, String lastName, String mail,String password){
+        String sql = "INSERT INTO USERS(name, lastName, mail, password) VALUES (?,?,?,?)";
+        createConexion();
+        try {
+            PreparedStatement psmt = cnn.prepareStatement(sql);
+            psmt.setString(1,name);
+            psmt.setString(2, lastName);
+            psmt.setString(3,mail);
+            psmt.setString(4, password);
+
+            psmt.executeUpdate();
+
+            System.out.println("User " +name+" has been created.");
+
+            psmt.close();
+        } catch (SQLException e) {
+            System.out.println("Error "+e.getErrorCode()+ " " +e.getMessage());
+        } finally {
+            try {
+                cnn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void updateUser(String name,String lastName, String mail, String password){
+        String sql = "UPDATE USERS SET name=?, lastName=?, password=? WHERE mail=?";
+        createConexion();
+        try {
+            PreparedStatement psmt = cnn.prepareStatement(sql);
+            psmt.setString(1,name);
+            psmt.setString(2,lastName);
+            psmt.setString(3,password);
+            psmt.setString(4,mail);
+
+            psmt.executeUpdate();
+            System.out.println("User has been updated.");
+
+            psmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                cnn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void deleteUser(String mail){
+        String sql = "UPDATE users set deletedAt = current_timestamp() where mail = ?";
+        createConexion();
+        try {
+            PreparedStatement stmt = cnn.prepareStatement(sql);
+            stmt.setString(1,mail);
+
+            int rowsAfected = stmt.executeUpdate();
+
+            if (rowsAfected > 0){
+                System.out.println("User has been deleted");
+            } else {
+                System.out.println("An error has occurred, user does not exist.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     public static void main(String[] args) {
         ConexionMysql conection = new ConexionMysql();
         conection.getUsers();
+        //conection.createUser("jhoana","torres aguirre","jhoana@gmail.com","1234");
+        //conection.updateUser("Jhoana","Torres Aguirre","jhoana@gmail.com","jhoa");
+        //.deleteUser("jhoana@gmail.com");
     }
 
 
